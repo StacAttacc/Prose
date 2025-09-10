cd7c70bb356e111102aa5c6bb37a2aca639757e1
Roberto Berrios <sky_dip_01@hotmail.com>
2025-09-10 11:09:42 -0400

    jaime mpas typescript

---
+++ src/test/java/com/AL565/prose/ProseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AL565.prose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
