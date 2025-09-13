2bbacd00fd927d25f3a6131716988e4d425f607a
Anastasia <cookiedemons@outlook.com>
2025-09-12 22:17:15 -0400

    feat: cv model

--- src/main/java/com/AL565/prose/model/ProseCV.java
+++ src/main/java/com/AL565/prose/model/ProseCV.java
@@ -1,4 +1,38 @@
 package com.AL565.prose.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(of = "id")
 public class ProseCV {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private long size;
+    private String lastModified;
+    private Instant lastModifiedDate;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(columnDefinition="bytea")
+    private byte[] data;
 }
