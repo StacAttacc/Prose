ce45cc34ce7244301e318c0f0d0f8a3c7c19e5d5
MokhttaAr <mrachdad@gmail.com>
2025-09-17 14:26:44 -0400

    Ajout de l'exception management dans le controlleur

--- src/main/java/com/AL565/prose/controller/EtudiantController.java
+++ src/main/java/com/AL565/prose/controller/EtudiantController.java
@@ -2,6 +2,7 @@ package com.AL565.prose.controller;
 
 import com.AL565.prose.service.EtudiantInscriptionService;
 import com.AL565.prose.service.dto.EtudiantDto;
+import com.AL565.prose.service.exception.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +18,19 @@ public class EtudiantController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Void> inscrireEtudiant(@RequestBody EtudiantDto dto) {
-        etudiantInscriptionService.inscrireEtudiant(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<String> inscrireEtudiant(@RequestBody EtudiantDto dto) {
+        try {
+            etudiantInscriptionService.inscrireEtudiant(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (EmailAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erreur lors de l'inscription");
+        }
     }
+
 
 
 
