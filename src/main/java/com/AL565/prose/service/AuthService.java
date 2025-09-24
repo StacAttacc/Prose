1a17d47cb902d8b895a03937859728d65f21ac88
luis fonmarty <luis@MacBook-Pro-de-utilisateur.local>
2025-09-24 15:17:43 -0400

    code review

--- src/main/java/com/AL565/prose/service/AuthService.java
+++ src/main/java/com/AL565/prose/service/AuthService.java
@@ -2,6 +2,9 @@ package com.AL565.prose.service;
 
 import com.AL565.prose.service.dto.LoginRequestDTO;
 import com.AL565.prose.service.dto.ProseUserDTO;
+
+import lombok.RequiredArgsConstructor;
+
 import com.AL565.prose.security.JwtTokenProvider;
 import com.AL565.prose.model.ProseUser;
 import com.AL565.prose.repository.ProseUserRepository;
@@ -11,31 +14,24 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final ProseUserRepository userRepository;
 
-    public AuthService(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, ProseUserRepository userRepository) {
-        this.authenticationManager = authenticationManager;
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userRepository = userRepository;
-    }
-
     public ProseUserDTO login(LoginRequestDTO request) {
-        // Authentifie l'utilisateur
+        
         Authentication authentication = authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
                 request.getEmail(), 
                 request.getPassword()
             )
         );
-
-        // Génére le token JWT
+ 
         String token = jwtTokenProvider.generateToken(authentication);
         
-        // Récupére l'utilisateur complet
         ProseUser user = userRepository.findByCredentials_Username(request.getEmail())
             .orElseThrow(() -> new RuntimeException("User not found"));
 
