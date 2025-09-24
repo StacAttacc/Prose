94598a58ac4d20f672fc2160346a60e59a924e24
luis fonmarty <luis@MacBook-Pro-de-utilisateur.local>
2025-09-23 21:43:29 -0400

    correction du test

--- src/main/java/com/AL565/prose/ProseApplication.java
+++ src/main/java/com/AL565/prose/ProseApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @AllArgsConstructor
 @SpringBootApplication
@@ -16,12 +15,11 @@ public class ProseApplication {
 
     private final EmployeurService employeurService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProseApplication.class, args);
+    }
 
     @Bean
-    @Profile("!test")
     public CommandLineRunner run() {
         return args -> {
             EmployeurEnregistrerDTO employeurMark = new EmployeurEnregistrerDTO(
