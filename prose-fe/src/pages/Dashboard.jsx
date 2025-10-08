dfea921bfd55fe87aa35cd403103379c922404c0
LuisFonmarty <luis.fonmarty@gmail.com>
2025-10-07 23:14:27 -0400

    suppression de TeleversementCV

--- prose-fe/src/pages/Dashboard.jsx
+++ prose-fe/src/pages/Dashboard.jsx
@@ -63,9 +63,6 @@ export default function Dashboard() {
                                 <button onClick={() => {nav('etudiant/mon-cv')}}>
                                     Mon CV
                                 </button>
-                                <button onClick={() => {nav('etudiant/TeleversementCV')}}>
-                                    Téléverser mon CV
-                                </button>
                                 <button onClick={() => {nav('stage-listings')}} className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
                                     Stages Disponibles
                                 </button>
