ce0ce133e9f2b35462cdc86e23e9708d7cee17dc
Roberto Berrios <sky_dip_01@hotmail.com>
2025-10-10 14:46:12 -0400

    changement du bouton retour aux offre pour red au lieu de teal

--- prose-fe/src/components/employeur-components/StageApplicants.jsx
+++ prose-fe/src/components/employeur-components/StageApplicants.jsx
@@ -148,7 +148,7 @@ const StageApplicantsPage = ({ id, user, updateApplicantStatus, fetchApplicants 
 
             <NavLink
                 to="/employeur/posted-stages"
-                className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
             >
                 Retour aux offres
             </NavLink>
