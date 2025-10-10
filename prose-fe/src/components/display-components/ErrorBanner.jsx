dd6f86f0ac6670c74029eb31f6b022d47b8e63d4
Zacharie <Zacharie.Bouchard16@gmail.com>
2025-10-10 12:19:07 -0400

    Changement de la couleur du texte et rouge et palissement du background pour rendre le texte visible.

--- prose-fe/src/components/display-components/ErrorBanner.jsx
+++ prose-fe/src/components/display-components/ErrorBanner.jsx
@@ -1,6 +1,6 @@
 export default function ErrorBanner({ message }) {
     return (
-        <div className="mb-4 rounded-lg border border-rose-600 bg-rose-900/30 p-3 text-black">
+        <div className="mb-4 rounded-lg border border-rose-600 bg-rose-900/15 p-3 text-rose-800">
             {message}
         </div>
     )
