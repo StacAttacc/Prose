b016b53e604123b6de8496c1949a15963a9eb752
Anastasia <cookiedemons@outlook.com>
2025-10-18 14:54:18 -0400

    feat: postulation stage model

---
+++ src/main/java/com/AL565/prose/model/notifications/PostulationNotification.java
@@ -0,0 +1,22 @@
+package com.AL565.prose.model.notifications;
+
+import com.AL565.prose.model.Candidature;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostulationNotification extends Notification {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "candidature_id")
+    private Candidature candidature;
+}
