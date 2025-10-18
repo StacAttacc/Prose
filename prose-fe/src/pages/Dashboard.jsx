b51a1e7ec64b83cbaed690d5760dcd511a8307e3
LuisFonmarty <luis.fonmarty@gmail.com>
2025-10-18 16:54:06 -0400

    bug bouton lettre motivation corrigé

--- prose-fe/src/pages/Dashboard.jsx
+++ prose-fe/src/pages/Dashboard.jsx
@@ -80,7 +80,7 @@ export default function Dashboard() {
                                     Mon CV
                                 </button>
                                 <button onClick={() => {nav('etudiant/stages')}} className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
-                                    Mes Stages
+                                    Stages
                                 </button>
                             </>
                         ) : <></>}
