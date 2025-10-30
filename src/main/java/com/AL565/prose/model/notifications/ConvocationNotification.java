195ad2b17629e3a35ccee0e848e2a45e2944b67f
Anastasia <cookiedemons@outlook.com>
2025-10-30 16:11:57 -0400

    feat: candidature notification

---
+++ src/main/java/com/AL565/prose/model/notifications/ConvocationNotification.java
@@ -0,0 +1,16 @@
+package com.AL565.prose.model.notifications;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConvocationNotification extends Notification {
+    private String candidatureReference;
+}
