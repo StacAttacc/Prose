23c07458df26b46243ad843af52cec1445daf970
MokhttaAr <mrachdad@gmail.com>
2025-10-31 15:25:42 -0400

    fix: ajout @DiscriminatorValue pour corriger contrainte notification_type_check

--- src/main/java/com/AL565/prose/model/notifications/GestionnaireCvNotification.java
+++ src/main/java/com/AL565/prose/model/notifications/GestionnaireCvNotification.java
@@ -1,12 +1,14 @@
 package com.AL565.prose.model.notifications;
 
 import com.AL565.prose.model.CV;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.*;
 
 @Entity
+@DiscriminatorValue("gestionnaire_cv")
 @Getter
 @Setter
 @NoArgsConstructor
--- src/main/java/com/AL565/prose/model/notifications/StageNotification.java
+++ src/main/java/com/AL565/prose/model/notifications/StageNotification.java
@@ -1,12 +1,14 @@
 package com.AL565.prose.model.notifications;
 
 import com.AL565.prose.model.Stage;
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.*;
 
 @Entity
+@DiscriminatorValue("stage")
 @Getter
 @Setter
 @NoArgsConstructor
