05d99a059c0a99506872ea5f6f2cc24f41750a6b
Roberto Berrios <sky_dip_01@hotmail.com>
2025-10-31 14:45:29 -0400

    ajout du mapper getCandidatures dans le back end

--- src/main/java/com/AL565/prose/model/CandidatureStatus.java
+++ src/main/java/com/AL565/prose/model/CandidatureStatus.java
@@ -1,11 +1,8 @@
 package com.AL565.prose.model;
 
 public enum CandidatureStatus {
-    SOUMISE("Soumise"),
-    ACCEPTEE("Acceptee"),
-    CONVOQUEE("Convoquee"),
-    REFUSEE("Refusee");
-
-    CandidatureStatus(String description) {}
-
+    SOUMISE,
+    ACCEPTEE,
+    CONVOQUEE,
+    REFUSEE
 }
