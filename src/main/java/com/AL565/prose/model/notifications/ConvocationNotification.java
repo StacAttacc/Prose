2bc9804ee438a26ca7bb3f2305c01dd8a10bdf6c
Anastasia <cookiedemons@outlook.com>
2025-10-31 14:26:16 -0400

    feat: new type of notification

--- src/main/java/com/AL565/prose/model/notifications/ConvocationNotification.java
+++ src/main/java/com/AL565/prose/model/notifications/ConvocationNotification.java
@@ -12,5 +12,5 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConvocationNotification extends Notification {
-    private String candidatureReference;
+    private Long candidatureConvocationId;
 }
