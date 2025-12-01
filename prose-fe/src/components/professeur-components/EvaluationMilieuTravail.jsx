caf511ca8f83a07558d85f10353c9c8c52ad0865
Anastasia <cookiedemons@outlook.com>
2025-12-01 01:50:39 -0500

    fix: can see the save button again

--- prose-fe/src/components/professeur-components/EvaluationMilieuTravail.jsx
+++ prose-fe/src/components/professeur-components/EvaluationMilieuTravail.jsx
@@ -578,7 +578,7 @@ export default function EvaluationMilieuTravail() {
                         >
                             {t('common.cancel') || 'Annuler'}
                         </button>
-                        {viewing && (
+                        {!viewing && (
                             <button
                                 type="submit"
                                 className="flex-1 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 flex items-center justify-center"
